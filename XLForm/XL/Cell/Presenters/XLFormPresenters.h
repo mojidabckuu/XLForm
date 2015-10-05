cfadfbe6968bb1e9457fc7ac21f2734698fcff15
mojidabckuu <mojidabckuu.22.06.92@gmail.com>
2015-10-05 09:48:25 +0400

    Refactoring folder structure for presenter.

---
+++ XLForm/XL/Cell/Presenters/XLFormPresenters.h
@@ -0,0 +1,19 @@
+//
+//  XLFormPresenters.h
+//  Pods
+//
+//  Created by vlad gorbenko on 10/5/15.
+//
+//
+
+#ifndef XLFormPresenters_h
+#define XLFormPresenters_h
+
+#import "XLFormSeguePresenter.h"
+#import "XLFormModalPresenter.h"
+#import "XLFormPushPresenter.h"
+#import "XLFormPopoverPresenter.h"
+#import "XLFormActionSheetPresenter.h"
+#import "XLFormAlertViewPresenter.h"
+
+#endif /* XLFormPresenters_h */
