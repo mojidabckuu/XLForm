3260601524f34d3436bb80c53d3613d9eda448bf
mojidabckuu <mojidabckuu.22.06.92@gmail.com>
2015-10-05 09:33:22 +0400

    Realise push presenter.

--- XLForm/XL/Cell/Presenters/XLFormPushPresenter/XLFormPushPresenter.m
+++ XLForm/XL/Cell/Presenters/XLFormPushPresenter/XLFormPushPresenter.m
@@ -8,6 +8,18 @@
 
 #import "XLFormPushPresenter.h"
 
+#import "XLFormRowDescriptor.h"
+
+#import "XLFormRowDescriptorViewController.h"
+
 @implementation XLFormPushPresenter
+
+- (void)presentWithCompletionBlock:(void (^)(void))completionBlock {
+    NSAssert([self.destinationViewController conformsToProtocol:@protocol(XLFormRowDescriptorViewController)], @"rowDescriptor.action.viewControllerClass must conform to XLFormRowDescriptorViewController protocol");
+    UIViewController<XLFormRowDescriptorViewController> *selectorViewController = (UIViewController<XLFormRowDescriptorViewController> *)self.destinationViewController;
+    selectorViewController.rowDescriptor = self.rowDescriptor;
+    selectorViewController.title = self.rowDescriptor.selectorTitle;
+    [self.sourceViewController.navigationController pushViewController:selectorViewController animated:YES];
+}
 
 @end
