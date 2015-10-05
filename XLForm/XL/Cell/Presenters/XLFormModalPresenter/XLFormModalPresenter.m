6b809c8e3f5da01f48c7dea4adf684de2c714028
mojidabckuu <mojidabckuu.22.06.92@gmail.com>
2015-10-05 09:33:30 +0400

    Realise modal presenter.

--- XLForm/XL/Cell/Presenters/XLFormModalPresenter/XLFormModalPresenter.m
+++ XLForm/XL/Cell/Presenters/XLFormModalPresenter/XLFormModalPresenter.m
@@ -8,6 +8,33 @@
 
 #import "XLFormModalPresenter.h"
 
+#import "XLFormRowDescriptor.h"
+
+#import "XLFormRowDescriptorViewController.h"
+
 @implementation XLFormModalPresenter
+
+- (void)presentWithCompletionBlock:(void (^)(void))completionBlock {
+    NSAssert([self.destinationViewController conformsToProtocol:@protocol(XLFormRowDescriptorViewController)], @"rowDescriptor.action.viewControllerClass must conform to XLFormRowDescriptorViewController protocol");
+    UIViewController<XLFormRowDescriptorViewController> *selectorViewController = (UIViewController<XLFormRowDescriptorViewController> *)self.destinationViewController;
+    selectorViewController.rowDescriptor = self.rowDescriptor;
+    selectorViewController.title = self.rowDescriptor.selectorTitle;
+    
+    id controller = selectorViewController;
+    if(![self.destinationViewController isKindOfClass:[UINavigationController class]]) {
+        UINavigationController *navigationController = [[UINavigationController alloc] initWithRootViewController:selectorViewController];
+        UIBarButtonItem *doneBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:NSLocalizedString(@"Done", nil) style:UIBarButtonItemStyleDone target:self action:@selector(dismiss:)];
+        selectorViewController.navigationItem.rightBarButtonItem = doneBarButtonItem;
+        controller = navigationController;
+    }
+    
+    [self.sourceViewController.navigationController pushViewController:controller animated:YES];
+}
+
+#pragma mark - User interaction
+
+- (void)dismiss:(id)sender {
+    [self.destinationViewController dismissViewControllerAnimated:YES completion:nil];
+}
 
 @end
