55760af3afa5aac2fd352bd61a1e6c329898c1a5
mojidabckuu <mojidabckuu.22.06.92@gmail.com>
2015-10-05 09:37:39 +0400

    Add segue presenter.

---
+++ XLForm/XL/Cell/Presenters/XLFormSeguePresenter/XLFormSeguePresenter.h
@@ -0,0 +1,13 @@
+//
+//  XLFormSeguePresenter.h
+//  Pods
+//
+//  Created by vlad gorbenko on 10/5/15.
+//
+//
+
+#import "XLFormPresenter.h"
+
+@interface XLFormSeguePresenter : XLFormPresenter
+
+@end
---
+++ XLForm/XL/Cell/Presenters/XLFormSeguePresenter/XLFormSeguePresenter.m
@@ -0,0 +1,30 @@
+//
+//  XLFormSeguePresenter.m
+//  Pods
+//
+//  Created by vlad gorbenko on 10/5/15.
+//
+//
+
+#import "XLFormSeguePresenter.h"
+
+#import "XLFormRowDescriptor.h"
+
+#import "XLFormRowDescriptorViewController.h"
+
+@implementation XLFormSeguePresenter
+
+- (void)presentWithCompletionBlock:(void (^)(void))completionBlock {
+    if (self.rowDescriptor.action.formSegueIdenfifier){
+        [self.sourceViewController performSegueWithIdentifier:self.rowDescriptor.action.formSegueIdenfifier sender:self.rowDescriptor];
+    }
+    else if (self.rowDescriptor.action.formSegueClass){
+        NSAssert(self.sourceViewController, @"either rowDescriptor.action.viewControllerClass or rowDescriptor.action.viewControllerStoryboardId or rowDescriptor.action.viewControllerNibName must be assigned");
+        NSAssert([self.sourceViewController conformsToProtocol:@protocol(XLFormRowDescriptorViewController)], @"selector view controller must conform to XLFormRowDescriptorViewController protocol");
+        UIStoryboardSegue * segue = [[self.rowDescriptor.action.formSegueClass alloc] initWithIdentifier:self.rowDescriptor.tag source:self.sourceViewController destination:self.sourceViewController];
+        [self.sourceViewController prepareForSegue:segue sender:self.rowDescriptor];
+        [segue perform];
+    }
+}
+
+@end
