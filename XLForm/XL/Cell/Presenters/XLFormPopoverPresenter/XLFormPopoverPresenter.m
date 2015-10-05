a20fc8340cfe47c2b7af9868d79d877c89e180f5
mojidabckuu <mojidabckuu.22.06.92@gmail.com>
2015-10-05 09:22:55 +0400

    Realise popover presentation.

--- XLForm/XL/Cell/Presenters/XLFormPopoverPresenter/XLFormPopoverPresenter.m
+++ XLForm/XL/Cell/Presenters/XLFormPopoverPresenter/XLFormPopoverPresenter.m
@@ -8,6 +8,48 @@
 
 #import "XLFormPopoverPresenter.h"
 
+#import "XLFormRowDescriptor.h"
+
+#import "XLFormRowDescriptorViewController.h"
+
+@interface XLFormPopoverPresenter () <UIPopoverControllerDelegate>
+
+@property (nonatomic, strong) UIPopoverController *popoverController;
+
+@end
+
 @implementation XLFormPopoverPresenter
+
+- (void)presentWithCompletionBlock:(void (^)(void))completionBlock {
+    NSAssert([self.destinationViewController conformsToProtocol:@protocol(XLFormRowDescriptorViewController)], @"rowDescriptor.action.viewControllerClass must conform to XLFormRowDescriptorViewController protocol");
+    UIViewController<XLFormRowDescriptorViewController> *selectorViewController = (UIViewController<XLFormRowDescriptorViewController> *)self.destinationViewController;
+    selectorViewController.rowDescriptor = self.rowDescriptor;
+    selectorViewController.title = self.rowDescriptor.selectorTitle;
+    
+    if (self.popoverController && self.popoverController.popoverVisible) {
+        [self.popoverController dismissPopoverAnimated:NO];
+    }
+    self.popoverController = [[UIPopoverController alloc] initWithContentViewController:selectorViewController];
+    self.popoverController.delegate = self;
+    if ([selectorViewController conformsToProtocol:@protocol(XLFormRowDescriptorPopoverViewController)]){
+        ((id<XLFormRowDescriptorPopoverViewController>)selectorViewController).popoverController = self.popoverController;
+    }
+    UITableViewCell *cell = [self.rowDescriptor cellForFormController:self.sourceViewController];
+    // TODO: add realistion to attached Rect.
+//        if (self.detailTextLabel.window){
+//            [self.popoverController presentPopoverFromRect:CGRectMake(0, 0, self.detailTextLabel.frame.size.width, self.detailTextLabel.frame.size.height) inView:self.detailTextLabel permittedArrowDirections:UIPopoverArrowDirectionAny animated:YES];
+//        }
+//        else{
+    // TODO: add arrow direction
+    [self.popoverController presentPopoverFromRect:cell.bounds inView:cell permittedArrowDirections:UIPopoverArrowDirectionAny animated:YES];
+    //        }
+    [self.sourceViewController.tableView deselectRowAtIndexPath:[self.sourceViewController.tableView indexPathForCell:cell] animated:YES];
+}
+
+#pragma mark - UIPopoverController delegate
+
+- (void)popoverControllerDidDismissPopover:(UIPopoverController *)popoverController {
+    [self.sourceViewController.tableView reloadData];
+}
 
 @end
