4601850a0e8cdfed714bf2d11b373d635df8cd4e
mojidabckuu <mojidabckuu.22.06.92@gmail.com>
2015-10-26 15:28:31 +0400

    Add storage implementation.

--- XLForm/XL/Models/XLRowTypesStorage/XLRowTypesStorage.h
+++ XLForm/XL/Models/XLRowTypesStorage/XLRowTypesStorage.h
@@ -10,4 +10,7 @@
 
 @interface XLRowTypesStorage : NSObject
 
++(NSMutableDictionary *)cellClassesForRowDescriptorTypes;
++(NSMutableDictionary *)inlineRowDescriptorTypesForRowDescriptorTypes;
+
 @end
--- XLForm/XL/Models/XLRowTypesStorage/XLRowTypesStorage.m
+++ XLForm/XL/Models/XLRowTypesStorage/XLRowTypesStorage.m
@@ -8,6 +8,79 @@
 
 #import "XLRowTypesStorage.h"
 
+#import "XLForm.h"
+
 @implementation XLRowTypesStorage
+
+#pragma mark - CellClasses
+
++(NSMutableDictionary *)cellClassesForRowDescriptorTypes
+{
+    static NSMutableDictionary * _cellClassesForRowDescriptorTypes;
+    
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        _cellClassesForRowDescriptorTypes = [@{
+                                               //                                               XLFormRowDescriptorTypeText:[XLFormTextFieldCell class],
+                                               //                                               XLFormRowDescriptorTypeName: [XLFormTextFieldCell class],
+                                               //                                               XLFormRowDescriptorTypePhone:[XLFormTextFieldCell class],
+                                               //                                               XLFormRowDescriptorTypeURL:[XLFormTextFieldCell class],
+                                               //                                               XLFormRowDescriptorTypeEmail: [XLFormTextFieldCell class],
+                                               //                                               XLFormRowDescriptorTypeTwitter: [XLFormTextFieldCell class],
+                                               //                                               XLFormRowDescriptorTypeAccount: [XLFormTextFieldCell class],
+                                               //                                               XLFormRowDescriptorTypePassword: [XLFormTextFieldCell class],
+                                               //                                               XLFormRowDescriptorTypeNumber: [XLFormTextFieldCell class],
+                                               //                                               XLFormRowDescriptorTypeInteger: [XLFormTextFieldCell class],
+                                               //                                               XLFormRowDescriptorTypeDecimal: [XLFormTextFieldCell class],
+                                               //                                               XLFormRowDescriptorTypeZipCode: [XLFormTextFieldCell class],
+                                               //                                               XLFormRowDescriptorTypeSelectorPush: [XLFormSelectorCell class],
+                                               //                                               XLFormRowDescriptorTypeSelectorPopover: [XLFormSelectorCell class],
+                                               //                                               XLFormRowDescriptorTypeSelectorActionSheet: [XLFormSelectorCell class],
+                                               //                                               XLFormRowDescriptorTypeSelectorAlertView: [XLFormSelectorCell class],
+                                               //                                               XLFormRowDescriptorTypeSelectorPickerView: [XLFormSelectorCell class],
+                                               //                                               XLFormRowDescriptorTypeSelectorPickerViewInline: [XLFormInlineSelectorCell class],
+                                               //                                               XLFormRowDescriptorTypeSelectorSegmentedControl: [XLFormSegmentedCell class],
+                                               //                                               XLFormRowDescriptorTypeMultipleSelector: [XLFormSelectorCell class],
+                                               //                                               XLFormRowDescriptorTypeMultipleSelectorPopover: [XLFormSelectorCell class],
+                                               //                                               XLFormRowDescriptorTypeTextView: [XLFormTextViewCell class],
+                                               //                                               XLFormRowDescriptorTypeButton: [XLFormButtonCell class],
+                                               //                                               XLFormRowDescriptorTypeInfo: [XLFormSelectorCell class],
+                                               XLFormRowDescriptorTypeBooleanSwitch : [XLFormSwitchCell class],
+                                               XLFormRowDescriptorTypeBooleanCheck : [XLFormCheckCell class],
+                                               XLFormRowDescriptorTypeDate: [XLFormDateCell class],
+                                               XLFormRowDescriptorTypeTime: [XLFormDateCell class],
+                                               XLFormRowDescriptorTypeDateTime : [XLFormDateCell class],
+                                               XLFormRowDescriptorTypeCountDownTimer : [XLFormDateCell class],
+                                               XLFormRowDescriptorTypeDateInline: [XLFormDateCell class],
+                                               XLFormRowDescriptorTypeTimeInline: [XLFormDateCell class],
+                                               XLFormRowDescriptorTypeDateTimeInline: [XLFormDateCell class],
+                                               XLFormRowDescriptorTypeCountDownTimerInline : [XLFormDateCell class],
+                                               XLFormRowDescriptorTypeDatePicker : [XLFormDatePickerCell class],
+                                               XLFormRowDescriptorTypePicker : [XLFormPickerCell class],
+                                               XLFormRowDescriptorTypeSlider : [XLFormSliderCell class],
+                                               XLFormRowDescriptorTypeStepCounter: [XLFormStepCounterCell class]
+                                               } mutableCopy];
+    });
+    return _cellClassesForRowDescriptorTypes;
+}
+
+#pragma mark - inlineRowDescriptorTypes
+
++(NSMutableDictionary *)inlineRowDescriptorTypesForRowDescriptorTypes
+{
+    static NSMutableDictionary * _inlineRowDescriptorTypesForRowDescriptorTypes;
+    
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        _inlineRowDescriptorTypesForRowDescriptorTypes = [
+                                                          @{XLFormRowDescriptorTypeSelectorPickerViewInline: XLFormRowDescriptorTypePicker,
+                                                            XLFormRowDescriptorTypeDateInline: XLFormRowDescriptorTypeDatePicker,
+                                                            XLFormRowDescriptorTypeDateTimeInline: XLFormRowDescriptorTypeDatePicker,
+                                                            XLFormRowDescriptorTypeTimeInline: XLFormRowDescriptorTypeDatePicker,
+                                                            XLFormRowDescriptorTypeCountDownTimerInline: XLFormRowDescriptorTypeDatePicker
+                                                            } mutableCopy];
+    });
+    return _inlineRowDescriptorTypesForRowDescriptorTypes;
+}
 
 @end
