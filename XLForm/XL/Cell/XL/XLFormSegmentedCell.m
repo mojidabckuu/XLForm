4b5f17187486dd77d14ef7adab294052595e3f3c
Alex <Alex@MacBook-Pro-Alex.local>
2016-08-31 20:53:07 +0400

    Segmented Cell changes

--- XLForm/XL/Cell/XL/XLFormSegmentedCell.m
+++ XLForm/XL/Cell/XL/XLFormSegmentedCell.m
@@ -29,14 +29,9 @@
 
 @interface XLFormSegmentedCell()
 
-@property NSMutableArray * dynamicCustomConstraints;
-
 @end
 
 @implementation XLFormSegmentedCell
-
-@synthesize textLabel = _textLabel;
-@synthesize segmentedControl = _segmentedControl;
 
 #pragma mark - XLFormDescriptorCell
 
@@ -44,50 +39,16 @@
 {
     [super configure];
     self.selectionStyle = UITableViewCellSelectionStyleNone;
-    [self.contentView addSubview:self.segmentedControl];
-    [self.contentView addSubview:self.textLabel];
-    [self.textLabel addObserver:self forKeyPath:@"text" options:NSKeyValueObservingOptionOld | NSKeyValueObservingOptionNew context:0];
     [self.segmentedControl addTarget:self action:@selector(valueChanged) forControlEvents:UIControlEventValueChanged];
 }
 
 -(void)update
 {
     [super update];
-    self.textLabel.text = [NSString stringWithFormat:@"%@%@", self.rowDescriptor.title, self.rowDescriptor.required && self.rowDescriptor.sectionDescriptor.formDescriptor.addAsteriskToRequiredRowsTitle ? @"*" : @""];
+    self.titleLabel.text = self.rowDescriptor.title;
     [self updateSegmentedControl];
     self.segmentedControl.selectedSegmentIndex = [self selectedIndex];
     self.segmentedControl.enabled = !self.rowDescriptor.isDisabled;
-}
-
-#pragma mark - KVO
-
--(void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context
-{
-    if (object == self.textLabel && [keyPath isEqualToString:@"text"]){
-        if ([[change objectForKey:NSKeyValueChangeKindKey] isEqualToNumber:@(NSKeyValueChangeSetting)]){
-            [self.contentView setNeedsUpdateConstraints];
-        }
-    }
-}
-
-#pragma mark - Properties
-
--(UISegmentedControl *)segmentedControl
-{
-    if (_segmentedControl) return _segmentedControl;
-    
-    _segmentedControl = [UISegmentedControl autolayoutView];
-    [_segmentedControl setContentHuggingPriority:500 forAxis:UILayoutConstraintAxisHorizontal];
-    return _segmentedControl;
-}
-
--(UILabel *)textLabel
-{
-    if (_textLabel) return _textLabel;
-    _textLabel = [UILabel autolayoutView];
-    [_textLabel setContentCompressionResistancePriority:500
-                                            forAxis:UILayoutConstraintAxisHorizontal];
-    return _textLabel;
 }
 
 #pragma mark - Action
@@ -121,47 +82,6 @@
         return index;
     }
     return UISegmentedControlNoSegment;
-}
-
-#pragma mark - Layout Constraints
-
-
--(void)updateConstraints
-{
-    if (self.dynamicCustomConstraints){
-        [self.contentView removeConstraints:self.dynamicCustomConstraints];
-    }
-    self.dynamicCustomConstraints = [NSMutableArray array];
-    NSDictionary * views = @{@"segmentedControl": self.segmentedControl, @"textLabel": self.textLabel};
-    if (self.textLabel.text.length > 0){
-        [self.dynamicCustomConstraints addObjectsFromArray:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-16-[textLabel]-16-[segmentedControl]-16-|"
-                                                                                options:NSLayoutFormatAlignAllCenterY
-                                                                                metrics:0
-                                                                                  views:views]];
-        [self.dynamicCustomConstraints addObjectsFromArray:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-12-[textLabel]-12-|"
-                                                                                options:0
-                                                                                metrics:0
-                                                                                  views:views]];
-        
-    }
-    else{
-        [self.dynamicCustomConstraints addObjectsFromArray:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-16-[segmentedControl]-16-|"
-                                                                                options:NSLayoutFormatAlignAllCenterY
-                                                                                metrics:0
-                                                                                  views:views]];
-        [self.dynamicCustomConstraints addObjectsFromArray:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-[segmentedControl]-|"
-                                                                                            options:0
-                                                                                                   metrics:0
-                                                                                                     views:views]];
-        
-    }
-    [self.contentView addConstraints:self.dynamicCustomConstraints];
-    [super updateConstraints];
-}
-
--(void)dealloc
-{
-    [self.textLabel removeObserver:self forKeyPath:@"text"];
 }
 
 @end
